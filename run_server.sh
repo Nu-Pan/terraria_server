c6be49742b263ea48d74475145af6d8bc0db6eec
NU-Pan <happy_hard-core@hotmail.co.jp>
2020-06-19 20:33:07 +0000

    git が改行コードを変更しやがったのでLFに戻した

--- run_server.sh
+++ run_server.sh
@@ -18,4 +18,16 @@ if [ $? -gt 0 ]; then
     exit 1
 fi
 
+# Choose World: n                           // New World
+# Choose size: 1                            // samll
+# Choose difficulty: 1                      // Classic
+# Enter world name: hello
+# Enter Seed (Leave Blank For Random):
+#
+# Choose World: 1                           // hello
+# Max players: 16
+# Server port: 7777
+# Automatically forward port? (y/n): y
+# Server password (press enter for none):
+
 exit 0
